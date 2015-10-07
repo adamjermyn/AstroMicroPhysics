d61d8d1735fec5a238ac6e23b55ae4cbaa7759fd
Adam Jermyn <adamjermyn@gmail.com>
2015-10-07 20:45:12 +0100

    Updated sourceClass to include more interpolation and blending options.

--- sourceClass.py
+++ sourceClass.py
@@ -1,4 +1,5 @@
 from scipy.interpolate import RegularGridInterpolator
+from scipy.interpolate import RectBivariateSpline
 from scipy.ndimage.filters import gaussian_filter
 import numpy as np
 
@@ -24,6 +25,10 @@ class source:
 			one plus the size of the first dimension of grid (i.e. grid.shape[0]+1). The
 			final dimension indexes the quantities of interest. The entries should be NaN when
 			data is unavailable.
+	linear 	-- Boolean specifying if the interpolation should be linear or not. Default is true.
+			THIS MUST BE SET TO TRUE FOR INTERPOLATION IN D>2. If False, the interpolation defaults
+			to bicubic splines.
+	smoothingDist -- This gives the distance over which the binary mask is smoothed in grid units. Default is 4.
 
 	For both input and output, a full specification of the available strings and the quantities
 	they correspond to is given in the file 'VariableDeclarations.md' in the top level of the
@@ -47,27 +52,40 @@ class source:
 	include tables which have data relevant to the point of interest.
 	"""
 
-	def __init__(self, grid, inNames, outNames, data):
+	def __init__(self, grid, inNames, outNames, data, linear=True, binaryMask=None, smoothingDist=4):
 
-		self.grid = grid
+		self.grid = np.copy(grid)
 		self.inNames = inNames
 		self.outNames = outNames
-		self.data =  data
+		self.data =  np.copy(data)
+		self.linear = linear
 
-		# Construct the binary mask
-		self.binaryMask = 1-1.0*np.isnan(data[...,0])
+		if binaryMask is None:
+			# Construct the binary mask
+			self.binaryMask = 1-1.0*np.isnan(data[...,0])
+		else:
+			self.binaryMask = np.copy(binaryMask)
 
 		# Construct the smooth mask
 		x = np.copy(self.binaryMask)
-		for i in range(4): 	# This procedure makes it smoothly drop off towards the edges, while being precisely
-							# zero once out of bounds.
+		# This procedure makes it smoothly drop off towards the edges, while being precisely
+		# zero once out of bounds.
+		for i in range(smoothingDist): 
 			x = gaussian_filter(x,sigma=1,order=0,mode='constant',cval=0)*x
-	    self.smoothMask = x
+		self.smoothMask = x/np.amax(x) # Ensures that smooth masks are equally weighted across sources
 
 		# Construct interpolators
-		self.interpolator = RegularGridInterpolator(grid,data,bounds_error=False, fill_value=0)
+		if linear:
+			self.interpolator = RegularGridInterpolator(grid,data,bounds_error=False, fill_value=0)
+		else:
+			data = np.copy(data)
+			data[np.isnan(data)] = 0
+			self.interpolator = [RectBivariateSpline(grid[0],grid[1],data[...,i],kx=3,ky=3) for i in range(data.shape[-1])]
+
+		# The mask interpolators are always linear. This ensures that we don't get oscillation, negative values, or other ill-conditioning.
 		self.binaryMaskInterpolataor = RegularGridInterpolator(grid,self.binaryMask,bounds_error=False, fill_value=0)
 		self.smoothMaskInterpolataor = RegularGridInterpolator(grid,self.smoothMask,bounds_error=False, fill_value=0)
+
 
 	def namedInterp(self,points,name):
 		"""
@@ -80,8 +98,14 @@ class source:
 		name   -- A string matching one of the strings in outNames. This specifies the quantity to
 				  output.
 		"""
-		ind = self.outNames.find(name)
-		return self.interpolator(points)[ind],self.binaryMaskInterpolataor(points),self.smoothMaskInterpolataor(points)
+		ind = self.outNames.index(name)
+		if ind == -1:
+			print 'Error: Requested name not available from this source.'
+			exit()
+		if self.linear:
+			return self.interpolator(points)[ind],self.binaryMaskInterpolataor(points),self.smoothMaskInterpolataor(points)
+		else:
+			return self.interpolator[ind](points[...,0],points[...,1],grid=False),self.binaryMaskInterpolataor(points),self.smoothMaskInterpolataor(points)
 
 	def interp(self,points):
 		"""
@@ -92,44 +116,29 @@ class source:
 				  If two it must have shape (N,grid.shape[0]), where N is the number of points
 				  specified.
 		"""
-		return self.interpolator(points),self.binaryMaskInterpolataor(points),self.smoothMaskInterpolataor(points)
+		if self.linear:
+			return self.interpolator(points),self.binaryMaskInterpolataor(points),self.smoothMaskInterpolataor(points)
+		else:
+			return self.interpolator(points[...,0],points[...,1],grid=False),self.binaryMaskInterpolataor(points),self.smoothMaskInterpolataor(points)
 
-def overlapSources(sources, points):
+def overlapNamedSources(sources, points, name, weights=None):
 	"""
 	Merges the output from multiple sources.
 
 	Arguments:
 	sources -- The source objects to merge.
 	points  -- The points at which to interpolate.
+	name 	-- The quantity to interpolate.
+	weights -- The weighting of the various sources, given as a numpy array of length len(sources).
+			   Default is None, which is processed as np.ones(len(sources)).
 
 	The sources are averaged, weighted based on their smoothMask values.
 	These values are zero out-of-bounds, and unity in the interior of the data set.
 	At the edges of the data set, the mask smoothly transitions between 1 and 0.
 	"""
-	output = []
-	masks = []
-	for s in sources:
-		out, _, smoothMask = s.interp(points)
-		output.append(out)
-		masks.append(smoothMask)
-	output = np.array(output)
-	masks = np.array(masks)
-	norm = np.sum(masks,axis=0)
-	out = np.sum(output*masks/norm,axis=0)
-	return out
+	if weights is None:
+		weights = np.ones(len(sources))
 
-def overlapNamedSources(sources, points, name):
-	"""
-	Merges the output from multiple sources.
-
-	Arguments:
-	sources -- The source objects to merge.
-	points  -- The points at which to interpolate.
-
-	The sources are averaged, weighted based on their smoothMask values.
-	These values are zero out-of-bounds, and unity in the interior of the data set.
-	At the edges of the data set, the mask smoothly transitions between 1 and 0.
-	"""
 	output = []
 	masks = []
 	for s in sources:
@@ -137,7 +146,7 @@ def overlapNamedSources(sources, points, name):
 		output.append(out)
 		masks.append(smoothMask)
 	output = np.array(output)
-	masks = np.array(masks)
+	masks = np.array(masks)*weights[:,np.newaxis]
 	norm = np.sum(masks,axis=0)
 	out = np.sum(output*masks/norm,axis=0)
 	return out
